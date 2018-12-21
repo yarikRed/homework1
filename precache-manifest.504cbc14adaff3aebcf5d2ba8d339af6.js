4f7647162e56ed00acd521aaa78031e406b26728
yarik <yarik.red@gmail.com>
2018-12-21 02:02:01 +0200

    Updates

---
+++ precache-manifest.504cbc14adaff3aebcf5d2ba8d339af6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "04190be9d1797d01cfc5",
+    "url": "/homework1/static/css/main.b28611fc.chunk.css"
+  },
+  {
+    "revision": "04190be9d1797d01cfc5",
+    "url": "/homework1/static/js/main.04190be9.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/homework1/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "8c1cd0fef087f1fe4b7c",
+    "url": "/homework1/static/js/runtime~main.8c1cd0fe.js"
+  },
+  {
+    "revision": "e844fe0a6cc261f4072eb651ee4893bc",
+    "url": "/homework1/index.html"
+  }
+];
